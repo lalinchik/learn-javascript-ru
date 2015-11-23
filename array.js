9cff9b3069a9e9b1d72d25e7845f10eec0286611
Alina <smaylik2410@gmail.com>
2015-11-23 22:57:39 +0200

    Removed some function

--- array.js
+++ array.js
@@ -1,31 +1,4 @@
 "use strict";
-/*var styles = ["Джаз", "Блюз"];
-styles.push("Рок-н-Ролл");
-styles[styles.length - 2] = "Классика";
-//alert(styles.shift());
-styles.unshift("Рэп", "Регги");
-
-var rand = Math.floor(Math.random() * styles.length);
-//alert(styles[rand]);
-
-
-
-var array = [];
-while(true){
-    //var number = prompt("Введите, пожалуйста, число", 0); щоб не заважало вирішувати інші задачі
-
-    if(number === "" || isNaN(number) || number === null){
-        break;
-    }
-
-    array.push(+number);
-}*/
-/*var sum = 0;
-for(var i = 0; i < array.length; i++){
-    sum += array[i];
-}*/
-//alert(sum);
-
 
 var arr = [];
 var max = 100;
