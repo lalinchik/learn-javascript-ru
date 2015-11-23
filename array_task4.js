5c01bafbadb1c3bb1dd54c429bfd63dfa29c7e0e
Alina <smaylik2410@gmail.com>
2015-11-23 23:06:09 +0200

    Added array_task4.js

---
+++ array_task4.js
@@ -0,0 +1,13 @@
+/**
+ * Created by Alina on 23.11.2015.
+ */
+function getMaxSubSum(arr) {
+    var maxSum = 0,
+        partialSum = 0;
+    for (var i = 0; i < arr.length; i++) {
+        partialSum += arr[i];
+        maxSum = Math.max(maxSum, partialSum);
+        if (partialSum < 0) partialSum = 0;
+    }
+    return maxSum;
+}
