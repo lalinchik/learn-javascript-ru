a02651753f76d28c99ab612b6154bfa075221e9c
Alina <smaylik2410@gmail.com>
2015-11-27 01:19:01 +0200

    Added object-methods.js

---
+++ functions_closures/object-methods.js
@@ -0,0 +1,36 @@
+var calculator = {
+    sum: function() {
+        return this.a + this.b;
+    },
+
+    mul: function() {
+        return this.a * this.b;
+    },
+
+    read: function() {
+        this.a = +prompt('a?', 0);
+        this.b = +prompt('b?', 0);
+    }
+};
+
+calculator.read();
+alert(calculator.sum());
+alert(calculator.mul());
+
+var ladder = {
+    step: 0,
+    up: function() {
+        this.step++;
+        return this;
+    },
+    down: function() {
+        this.step--;
+        return this;
+    },
+    showStep: function() {
+        alert( this.step );
+        return this;
+    }
+};
+
+ladder.up().up().down().up().down().showStep();
